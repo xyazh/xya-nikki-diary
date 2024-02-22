2517dc30734063b8bc28b1c512f2faadaff8f285
xyazh <330237917@qq.com>
2024-02-22 19:04:38 +0800

    修复了一些bug

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ try {
     app.password_manager.inputPassword(app.main);
 } catch (e) {
     console.error(e);
-    var stack_trace = e.stack.toString();
+    var stack_trace ="" + e + "\n" + e.stack.toString();
     files.ensureDir("/sdcard/xya日记/log/error.log");
     files.write("/sdcard/xya日记/log/error.log", stack_trace, [encoding = "utf-8"]);
     toast("发生严重错误,错误日志已写入：/sdcard/xya日记/log/error.log");
